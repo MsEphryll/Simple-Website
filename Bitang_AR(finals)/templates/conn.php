bff45ab5127c8b9d92a69b1a45b2ac3a46f906d2
Ephryll <135094289+MsEphryll@users.noreply.github.com>
2024-05-24 12:38:12 +0800

    nooo

--- Bitang_AR(finals)/templates/conn.php
+++ Bitang_AR(finals)/templates/conn.php
@@ -1,8 +1,8 @@
 <?php 
-$servername = "65.19.141.67";
-$username = "websys_abrilnotfound";
-$password = "four0four";
-$dbname = "websys_Bitang";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 // Create connection
 $conn =  mysqli_connect($servername, $username, $password, $dbname);
